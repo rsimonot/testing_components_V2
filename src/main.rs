07cd521b9df0f8c829bd70d85eb41b4aa73f76ea
Robin Simonot <rsimonot@magentinehealthcare.com>
2022-03-01 16:29:53 +0000

    OK for temperature

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,13 @@ use std::time::Duration;
 
 use rppal::gpio::Gpio;
 use rppal::system::DeviceInfo;
+use rppal::i2c::I2c;
 
 const FAN_PIN: u8 = 18;
 const PHOTO_PIN: u8 = 6;
+
+const TMP116_ADR: u16 = 0x48;
+const TEMP_REG: u8 = 0x00;
 
 fn trigger_fan() -> Result<(), Box<dyn Error>> {
     println!("Testing the fan on a {}.", DeviceInfo::new()?.model());
@@ -26,9 +30,25 @@ fn get_value_photosensor() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+fn i2c_get_temperature() -> Result<(), Box<dyn Error>> {
+    let mut i2c = I2c::new()?;
+    i2c.set_slave_address(TMP116_ADR)?;
+
+    i2c.write(&[TEMP_REG])?;
+
+    //let mut buffer = [0u8; 1];  // array of size 1 containing 0 as u8 value
+    //i2c.read(&mut buffer)?;
+
+    let temp = i2c.smbus_read_word(TEMP_REG)?;
+    println!("Read temperature => {}°C", (temp>>8|((temp<<8)&0xffff)) as f64 * 0.0078125);
+
+    Ok(())
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     //trigger_fan()?;
-    get_value_photosensor()?;
+    //get_value_photosensor()?;
+    i2c_get_temperature()?;
 
     Ok(())
 }
